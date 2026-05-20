d750b1369abb73daa2632bcc4d3e438f70c1bb5a
Manoel Neto <mneto_004@hotmail.com>
2026-05-20 16:21:02 -0300

    anal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import LoveTimeline from "./LoveTimeline";
 export default function App() {
   return (
